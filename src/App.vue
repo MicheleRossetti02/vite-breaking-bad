a9ee9291ad65928dfef6b0affa85120ba6611fbd
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-14 17:34:43 +0100

    card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script >
 import axios from "axios";
-
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import CharactersList from './components/CharactersList.vue';
@@ -38,10 +37,10 @@ export default {
 </script>
 
 <template>
-  <!-- 
+
   <div class="container">
-    <div class="row">
-      <div class="col" v-for="character in characters">
+    <div class="row row-cols-md-5">
+      <div class="col" v-for="character in store.characters">
         <div class="character text-center">
           <img :src="character.img" alt="" class="">
           <h3>{{ character.name }}</h3>
@@ -52,15 +51,18 @@ export default {
             <p>{{ character.status }}</p>
           </div>
         </div>
-      </div> -->
-  <!-- <CharactersList>
+      </div>
+      <!-- <CharactersList>
 
       </CharactersList> -->
-  <!-- 
+
+    </div>
   </div>
-  </div> -->
 </template>
 
 <style scoped>
+img {
+  width: 100%;
 
+}
 </style>
