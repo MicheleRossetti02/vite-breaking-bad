d642923cc8dc5fac4b9a88b4908f583e6dcba00c
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-15 17:37:45 +0100

    to fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <script>
+import SelectBox from './SelectBox.vue';
+
 export default {
-    name: 'AppHeader'
+    name: "AppHeader",
+    components: { SelectBox }
 }
 
 </script>
@@ -17,12 +20,9 @@ export default {
 
 
     <div class="select_category">
-        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-            <option selected>Select category</option>
-            <option value="1">Alive</option>
-            <option value="2">Deceased</option>
-            <option value="3">Presumed dead</option>
-        </select>
+
+        <SelectBox />
+
     </div>
 
 
---
+++ src/components/SelectBox.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "SelectBox"
+}
+const select = ({
+    data: {
+        key: ""
+    },
+    methods: {
+        onchange(event) {
+            console.log(event.target.value);
+        }
+    }
+})
+</script>
+
+<template>
+    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" v-model="key"
+        @change="onchange($event)">
+        <option selected>Select category</option>
+        <option value="1">Alive</option>
+        <option value="2">Deceased</option>
+        <option value="3">Presumed dead</option>
+    </select>
+</template>
